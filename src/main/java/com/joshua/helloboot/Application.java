004e1b31adb14982784c030cc47f5f6796d836f5
joshua <joshuara7235@gmail.com>
2023-02-14 23:47:24 +0900

    Containerless Standalone Application init

---
+++ src/main/java/com/joshua/helloboot/Application.java
@@ -0,0 +1,9 @@
+package com.joshua.helloboot;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Containerless Standalone Application");
+	}
+
+}
