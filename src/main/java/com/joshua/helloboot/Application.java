f4b81d0dab96f7422ba1a97250f250ce6d620869
joshua <joshuara7235@gmail.com>
2023-02-14 23:56:19 +0900

    Empty Tomcat Servlet Container Start (w/o Spring boot)

--- src/main/java/com/joshua/helloboot/Application.java
+++ src/main/java/com/joshua/helloboot/Application.java
@@ -1,9 +1,15 @@
 package com.joshua.helloboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Containerless Standalone Application");
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 
 }
