61f3d43794bf59c24481bdfb2c1786bfe28d0eab
joshua <joshuara7235@gmail.com>
2023-02-16 22:27:31 +0900

    Spring Container (ApplicationContext), Bean 등록 및 FrontController (Servlet Container) 에서 getBean

--- src/main/java/com/joshua/helloboot/Application.java
+++ src/main/java/com/joshua/helloboot/Application.java
@@ -3,6 +3,7 @@ package com.joshua.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -17,31 +18,25 @@ import java.io.IOException;
 public class Application {
 
 	public static void main(String[] args) {
+		GenericApplicationContext applicationContext = new GenericApplicationContext(); // Spring Container
+		applicationContext.registerBean(HelloController.class); // Bean 등록
+		applicationContext.refresh(); // Spring Container 가 본인이 가지고 있는 Configuration Metadata 를 통해 컨테이너를 초기화 하는 작업. -> Bean Object 를 만들어준다.
+
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-			HelloController helloController = new HelloController();
 
 			servletContext.addServlet("frontController", new HttpServlet() {
 				@Override
 				protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
 					// 인증, 보안, 다국어처리, 각종 공통 기능 (구현했다고 치고)
-
 					if (req.getRequestURI().equals("/hello") &&  req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
+						HelloController helloController = applicationContext.getBean(HelloController.class);// 등록한 Bean 을 가져온다. 가져오기만 하면된다.
 						String result = helloController.hello(name); // 복잡한 비지니스 로직은 여기에 위임
 
-						resp.setStatus(HttpStatus.OK.value());
-						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
 						resp.getWriter().println(result);
-					}
-					if (req.getRequestURI().equals("/hello") &&  req.getMethod().equals(HttpMethod.POST.name())) {
-						// post
-					}
-
-					else if (req.getRequestURI().equals("/user")) {
-						// code for /user
 					}
 					else {
 						resp.setStatus(HttpStatus.NOT_FOUND.value());
