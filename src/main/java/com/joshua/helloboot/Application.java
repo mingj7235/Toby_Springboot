56f5efccab8920671155b2c5a443b297a75df106
joshua <joshuara7235@gmail.com>
2023-02-18 14:50:02 +0900

    SpringApplication 으로 원복 (원리 파악)

--- src/main/java/com/joshua/helloboot/Application.java
+++ src/main/java/com/joshua/helloboot/Application.java
@@ -1,12 +1,11 @@
 package com.joshua.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -16,16 +15,11 @@ public class Application {
 	public ServletWebServerFactory servletWebServerFactory () {
 		return new TomcatServletWebServerFactory();
 	}
-
 	@Bean
 	public DispatcherServlet dispatcherServlet () {
 		return new DispatcherServlet();
 	}
-
 	public static void main(String[] args) {
-		MySpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
-
-
 }
