f936c1042f850f6a4f2e6e79c8460cd5140cdee6
joshua <joshuara7235@gmail.com>
2023-02-19 23:49:44 +0900

    SpringBootApplication 으로 가기 위한 과정 - Config 정보를 따로 뺀다. 즉, Servlet Container 와 Dispatcher Servlet 의 Bean 을 등록하는 로직을 Config 클래스로 뺀다.
    - Composed Annotation 인 MySpringBootApplication 을 생성하여 적용시킨다.

--- src/main/java/com/joshua/helloboot/Application.java
+++ src/main/java/com/joshua/helloboot/Application.java
@@ -1,24 +1,10 @@
 package com.joshua.helloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootApplication
 public class Application {
-	@Bean
-	public ServletWebServerFactory servletWebServerFactory () {
-		return new TomcatServletWebServerFactory();
-	}
-	@Bean
-	public DispatcherServlet dispatcherServlet () {
-		return new DispatcherServlet();
-	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
---
+++ src/main/java/com/joshua/helloboot/MySpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.joshua.helloboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE) //Class, Interface, Enum
+@Configuration
+@ComponentScan
+public @interface MySpringBootApplication {
+
+}
