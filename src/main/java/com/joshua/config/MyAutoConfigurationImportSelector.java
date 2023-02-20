ccd8f76f6cb83bd99992491290276a74875c7b20
joshua <joshuara7235@gmail.com>
2023-02-20 22:59:27 +0900

    Selector 와 MyAutoConfiguration 어노테이션을 통해 외부에서 구성 정보 후보 Bean 을 등록한다. (ClassPath)

--- src/main/java/com/joshua/config/MyAutoConfigurationImportSelector.java
+++ src/main/java/com/joshua/config/MyAutoConfigurationImportSelector.java
@@ -1,15 +1,26 @@
 package com.joshua.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigurationImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigurationImportSelector(final ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
 
     @Override
     public String[] selectImports(final AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "com.joshua.config.autoconfig.DispatcherServletConfig",
-                "com.joshua.config.autoconfig.TomcatWebServerConfiguration"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(String[]::new);
     }
 }
