a93f8b35937d7e8ae4d4ce0d26605d73e7f885c5
joshua <joshuara7235@gmail.com>
2023-02-24 22:28:25 +0900

    @Configuration Annotation Attribute : proxyBeanMethod

--- src/main/java/com/joshua/config/autoconfig/TomcatWebServerConfiguration.java
+++ src/main/java/com/joshua/config/autoconfig/TomcatWebServerConfiguration.java
@@ -1,11 +1,11 @@
 package com.joshua.config.autoconfig;
 
+import com.joshua.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfiguration {
     @Bean
     public ServletWebServerFactory servletWebServerFactory () {
